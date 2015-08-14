dced30aaa6b43a06898cd2918d5b41e5fc7f4ce4
Nick Butcher <nickbutcher@google.com>
2015-08-14 11:29:20 +0100

    Use lower quality images on low RAM devices. #TNB
    Change-Id: If2a6d84fdefcd5b957fc65c99e9f36c934eb63be

--- app/src/main/java/com/example/android/plaid/ui/util/glide/GlideConfiguration.java
+++ app/src/main/java/com/example/android/plaid/ui/util/glide/GlideConfiguration.java
@@ -16,6 +16,7 @@
 
 package com.example.android.plaid.ui.util.glide;
 
+import android.app.ActivityManager;
 import android.content.Context;
 
 import com.bumptech.glide.Glide;
@@ -24,18 +25,21 @@ import com.bumptech.glide.load.DecodeFormat;
 import com.bumptech.glide.module.GlideModule;
 
 /**
- * Created by nickbutcher on 3/26/15.
+ * Configure Glide to set desired image quality.
  */
 public class GlideConfiguration implements GlideModule {
 
     @Override
     public void applyOptions(Context context, GlideBuilder builder) {
-        // Apply options to the builder here.
-        builder.setDecodeFormat(DecodeFormat.PREFER_ARGB_8888);
+        // Prefer higher quality images unless we're on a low RAM device
+        ActivityManager activityManager =
+                (ActivityManager) context.getSystemService(Context.ACTIVITY_SERVICE);
+        builder.setDecodeFormat(activityManager.isLowRamDevice() ?
+                        DecodeFormat.PREFER_RGB_565 : DecodeFormat.PREFER_ARGB_8888);
     }
 
     @Override
     public void registerComponents(Context context, Glide glide) {
-        // register ModelLoaders here.
+
     }
 }
