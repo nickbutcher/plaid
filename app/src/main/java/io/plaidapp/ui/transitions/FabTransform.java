5fded59d42734c7cf1bf30c35a805991a59cdce0
MrWasdennnoch <leiter04@gmail.com>
2016-07-03 13:06:39 +0200

    Fix FabTransform circular reveal calculation
    This isn't really noticable at high speeds from a fab to a dialog, but slowed down you see that the transition from the fab to an activity cuts off after approx. 75% of the animation and just jumps to the non-clipped activity. With this fix the end radius gets calculated correctly. Same for the reversed transition.

--- app/src/main/java/io/plaidapp/ui/transitions/FabTransform.java
+++ app/src/main/java/io/plaidapp/ui/transitions/FabTransform.java
@@ -198,14 +198,14 @@ public class FabTransform extends Transition {
                     view.getWidth() / 2,
                     view.getHeight() / 2,
                     startBounds.width() / 2,
-                    (float) Math.hypot(endBounds.width() / 2, endBounds.width() / 2));
+                    (float) Math.hypot(endBounds.width() / 2, endBounds.height() / 2));
             circularReveal.setInterpolator(
                     AnimUtils.getFastOutLinearInInterpolator(sceneRoot.getContext()));
         } else {
             circularReveal = ViewAnimationUtils.createCircularReveal(view,
                     view.getWidth() / 2,
                     view.getHeight() / 2,
-                    (float) Math.hypot(startBounds.width() / 2, startBounds.width() / 2),
+                    (float) Math.hypot(startBounds.width() / 2, startBounds.height() / 2),
                     endBounds.width() / 2);
             circularReveal.setInterpolator(
                     AnimUtils.getLinearOutSlowInInterpolator(sceneRoot.getContext()));
