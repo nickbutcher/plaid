702203d1a7d316f942f70c1c8da83ea6d083f148
Florina Muntenescu <florinam@google.com>
2018-06-15 15:05:11 +0200

    Splitting the code even more

---
+++ app/src/main/java/io/plaidapp/designernews/data/api/DesignerNewsAuthTokenHolder.kt
@@ -0,0 +1,18 @@
+package io.plaidapp.designernews.data.api
+
+/**
+ * Holder for Designer News auth token. Since some APIs can be triggered without requiring
+ * authentication, the token can be missing.
+ */
+class DesignerNewsAuthTokenHolder(var authToken: String? = null) {
+    companion object {
+        @Volatile
+        private var INSTANCE: DesignerNewsAuthTokenHolder? = null
+
+        fun getInstance(authToken: String?): DesignerNewsAuthTokenHolder =
+                INSTANCE ?: synchronized(this) {
+                    INSTANCE
+                            ?: DesignerNewsAuthTokenHolder(authToken).also { INSTANCE = it }
+                }
+    }
+}
