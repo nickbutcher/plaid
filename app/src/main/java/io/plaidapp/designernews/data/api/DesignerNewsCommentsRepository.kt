c9f0fde0fc314c19a8c66bf1b4e3d6b9095d8145
Florina Muntenescu <florinam@google.com>
2018-06-17 15:21:25 +0200

    Adding comments DN API

--- app/src/main/java/io/plaidapp/designernews/data/api/DesignerNewsCommentsRepository.kt
+++ app/src/main/java/io/plaidapp/designernews/data/api/DesignerNewsCommentsRepository.kt
@@ -1,10 +1,41 @@
-package io.plaidapp.designernews.data.api.model
+package io.plaidapp.designernews.data.api
 
-import io.plaidapp.designernews.data.api.DesignerNewsService
+import android.util.Log
+import io.plaidapp.designernews.data.api.model.Comment
+import kotlinx.coroutines.experimental.launch
 
 class DesignerNewsCommentsRepository(private val service: DesignerNewsService) {
 
-    fun getComments(): List<Comment> {
-        
+    private var inProgress = false
+
+    fun getComments(
+            ids: List<String>,
+            onSuccess: (comments: List<Comment>) -> Unit,
+            onError: (error: String) -> Unit
+    ) {
+        val requestIds = ids.joinToString()
+        Log.d("flo", "ids $ids req $requestIds" )
+        inProgress = true
+        launch {
+            val result = service.getComments(requestIds).await()
+            if (result.isSuccessful && result.body() != null) {
+                onSuccess(result.body().orEmpty())
+            } else {
+                onError("Unable to get comments")
+            }
+            inProgress = false
+        }
+    }
+
+    companion object {
+        @Volatile
+        private var INSTANCE: DesignerNewsCommentsRepository? = null
+
+        fun getInstance(service: DesignerNewsService): DesignerNewsCommentsRepository {
+            return INSTANCE ?: synchronized(this) {
+                INSTANCE
+                        ?: DesignerNewsCommentsRepository(service).also { INSTANCE = it }
+            }
+        }
     }
 }
