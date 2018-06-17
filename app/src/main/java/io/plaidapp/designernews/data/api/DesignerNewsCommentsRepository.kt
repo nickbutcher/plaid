bfd395184399de8a8e86d42817894a7b1cfe4291
Florina Muntenescu <florinam@google.com>
2018-06-17 15:20:49 +0200

    Adding comments DN API

---
+++ app/src/main/java/io/plaidapp/designernews/data/api/DesignerNewsCommentsRepository.kt
@@ -0,0 +1,10 @@
+package io.plaidapp.designernews.data.api.model
+
+import io.plaidapp.designernews.data.api.DesignerNewsService
+
+class DesignerNewsCommentsRepository(private val service: DesignerNewsService) {
+
+    fun getComments(): List<Comment> {
+        
+    }
+}
