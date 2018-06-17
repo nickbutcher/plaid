daf1225dbc8e81fadb1a2a8feab4148ae3aa0e67
Florina Muntenescu <florinam@google.com>
2018-06-17 23:27:07 +0100

    Code cleaned up

--- app/src/test/java/io/plaidapp/designernews/login/data/fakes/FakeDesignerNewsLoginLocalDataSource.kt
+++
@@ -1,9 +0,0 @@
-package io.plaidapp.designernews.login.data.fakes
-
-import io.plaidapp.designernews.login.data.DesignerNewsLoginLocalDataSource
-
-class FakeDesignerNewsLoginLocalDataSource() : DesignerNewsLoginLocalDataSource() {
-    override fun clearData() {
-        // nothing to do here
-    }
-}
