e7cad161cd6c92cc06b5269a3c5f5274f3b13ce9
Ben Weiss <benweiss@google.com>
2018-06-19 13:49:28 +0100

    Move even more transitions to base
    DeparallaxingChangeBounds
    Pop

--- base/src/main/java/io/plaidapp/base/ui/transitions/Pop.java
+++ base/src/main/java/io/plaidapp/base/ui/transitions/Pop.java
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package io.plaidapp.ui.transitions;
+package io.plaidapp.base.ui.transitions;
 
 import android.animation.Animator;
 import android.animation.ObjectAnimator;
