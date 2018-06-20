743d78be6bae18a1313759dd1f220489f35a438c
Tiem Song <tiem@google.com>
2018-06-19 21:22:08 -0700

    Update config, add comment describing rename

--- ci_scripts/copy_lint_results.sh
+++ ci_scripts/copy_lint_results.sh
@@ -6,8 +6,9 @@ mkdir -p $RESULTS_DIR
 # Find all lint result files, except ones for third party libraries
 LINT_FILES=`find . -type f -regex ".*/build/reports/lint-results\..*" | grep -v third_party`
 
-# Rename each file. Example:
-
+# Rename each file and append the module name. Examples:
+# app/build/reports/lint-results.html -> app/build/reports/lint-results-app.html
+# base/build/reports/lint-results.html -> base/build/reports/lint-results-base.html
 for file in $LINT_FILES
 do
   rename 's/\/(.*)\/build\/reports\/lint-results/\/$1\/build\/reports\/lint-results-$1/' $file
