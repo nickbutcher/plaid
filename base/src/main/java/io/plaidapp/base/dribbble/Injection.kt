95b6088076bc72c91e923aedd2dc3e81fa6f31ee
Nick Butcher <nickbutcher@google.com>
2018-06-25 10:52:38 +0100

    Spotless formatting.

--- base/src/main/java/io/plaidapp/base/dribbble/Injection.kt
+++ base/src/main/java/io/plaidapp/base/dribbble/Injection.kt
@@ -1,18 +1,17 @@
 /*
- *   Copyright 2018 Google LLC
+ * Copyright 2018 Google, Inc.
  *
- *   Licensed under the Apache License, Version 2.0 (the "License");
- *   you may not use this file except in compliance with the License.
- *   You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *        http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
- *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 @file:JvmName("Injection")
@@ -31,25 +30,25 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 private val loggingInterceptor = HttpLoggingInterceptor().apply {
-  level = if (BuildConfig.DEBUG) {
-    BODY
-  } else {
-    NONE
-  }
+    level = if (BuildConfig.DEBUG) {
+        BODY
+    } else {
+        NONE
+    }
 }
 
 fun provideDribbbleService(): DribbbleService {
-  val client = OkHttpClient.Builder()
-      .addInterceptor(loggingInterceptor)
-      .build()
-  val gson = GsonBuilder()
-      .setDateFormat(DribbbleService.DATE_FORMAT)
-      .create()
-  return Retrofit.Builder()
-      .baseUrl(DribbbleService.ENDPOINT)
-      .client(client)
-      .addConverterFactory(DenvelopingConverter(gson))
-      .addConverterFactory(GsonConverterFactory.create(gson))
-      .build()
-      .create(DribbbleService::class.java)
+    val client = OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .build()
+    val gson = GsonBuilder()
+            .setDateFormat(DribbbleService.DATE_FORMAT)
+            .create()
+    return Retrofit.Builder()
+            .baseUrl(DribbbleService.ENDPOINT)
+            .client(client)
+            .addConverterFactory(DenvelopingConverter(gson))
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .build()
+            .create(DribbbleService::class.java)
 }
