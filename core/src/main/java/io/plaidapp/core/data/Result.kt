c23d57dfe37d620175d47b2c955677fa56a592a1
Florina Muntenescu <florinam@google.com>
2018-07-03 15:17:52 +0100

    Fixing review comments

--- core/src/main/java/io/plaidapp/core/data/Result.kt
+++ core/src/main/java/io/plaidapp/core/data/Result.kt
@@ -20,7 +20,7 @@ package io.plaidapp.core.data
  * A generic class that holds a value with its loading status.
  * @param <T>
  */
-sealed class Result<out R> {
+sealed class Result<out Any> {
 
     data class Success<out T>(val data: T) : Result<T>()
     data class Error(val exception: Exception) : Result<Nothing>()
