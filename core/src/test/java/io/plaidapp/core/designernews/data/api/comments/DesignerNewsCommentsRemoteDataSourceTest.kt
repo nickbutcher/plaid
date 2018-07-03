8b843e0ef06f578fbfd66eb26854e2dea04cf5a0
Florina Muntenescu <florinam@google.com>
2018-07-03 14:38:21 +0100

    Moving tests

---
+++ core/src/test/java/io/plaidapp/core/designernews/data/api/comments/DesignerNewsCommentsRemoteDataSourceTest.kt
@@ -0,0 +1,93 @@
+/*
+ * Copyright 2018 Google, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.plaidapp.core.designernews.data.api.comments
+
+import io.plaidapp.core.data.Result
+import io.plaidapp.core.data.isSuccessful
+import io.plaidapp.core.designernews.data.api.DesignerNewsService
+import io.plaidapp.core.designernews.data.api.model.Comment
+import kotlinx.coroutines.experimental.CompletableDeferred
+import kotlinx.coroutines.experimental.runBlocking
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertNotNull
+import org.junit.Test
+import org.mockito.Mockito
+import retrofit2.Response
+
+/**
+ * Tests for [DesignerNewsCommentsRemoteDataSource] that mock the Designer News API
+ */
+class DesignerNewsCommentsRemoteDataSourceTest {
+
+    private val service = Mockito.mock(DesignerNewsService::class.java)
+    private val dataSource = DesignerNewsCommentsRemoteDataSource(service)
+
+    @Test
+    fun getComments_whenRequestSuccessful() = runBlocking {
+        // Given that the service responds with success
+        val result = Response.success(replies)
+        Mockito.`when`(service.getComments("1")).thenReturn(CompletableDeferred(result))
+
+        // When getting the list of comments
+        val response = dataSource.getComments(listOf(1L))
+
+        // Then the response is the expected one
+        assertNotNull(response)
+        assertEquals(Result.Success(replies), response)
+    }
+
+    @Test
+    fun getComments_forMultipleComments() = runBlocking {
+        // Given that the service responds with success for specific ids
+        val result = Response.success(replies)
+        Mockito.`when`(service.getComments("11,12")).thenReturn(CompletableDeferred(result))
+
+        // When getting the list of comments for specific list of ids
+        val response = dataSource.getComments(listOf(11L, 12L))
+
+        // Then the response is the expected one
+        assertNotNull(response)
+        assertEquals(Result.Success(replies), response)
+    }
+
+    @Test
+    fun getComments_whenRequestFailed() = runBlocking {
+        // Given that the service responds with failure
+        val result = Response.error<List<Comment>>(400, errorResponseBody)
+        Mockito.`when`(service.getComments("1")).thenReturn(CompletableDeferred(result))
+
+        // When getting the list of comments
+        val response = dataSource.getComments(listOf(1L))
+
+        // Then the response is not successful
+        assertFalse(response.isSuccessful())
+    }
+
+    @Test
+    fun getComments_whenResponseEmpty() = runBlocking {
+        // Given that the service responds with success but with an empty response
+        val result = Response.success<List<Comment>>(null)
+        Mockito.`when`(service.getComments("1")).thenReturn(CompletableDeferred(result))
+
+        // When getting the list of comments
+        val response = dataSource.getComments(listOf(1L))
+
+        // Then the response is not successful
+        assertFalse(response.isSuccessful())
+    }
+}
