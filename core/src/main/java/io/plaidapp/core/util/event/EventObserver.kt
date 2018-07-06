101183ccd4dcf6a03fb7fa41446774be7bff7349
Ben Weiss <benweiss@google.com>
2018-07-06 11:00:40 +0100

    Introduce Event and EventObserver

---
+++ core/src/main/java/io/plaidapp/core/util/event/EventObserver.kt
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2018 Google LLC
+ *
+ * Licensed to the Apache Software Foundation (ASF) under one or more contributor
+ * license agreements. See the NOTICE file distributed with this work for
+ * additional information regarding copyright ownership. The ASF licenses this
+ * file to you under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package io.plaidapp.core.util.event
+
+import android.arch.lifecycle.Observer
+
+/**
+ * An [Observer] for [Event]s, simplifying the pattern of checking if the [Event]'s content has
+ * already been consumed.
+ *
+ * [onEventUnconsumedContent] is *only* called if the [Event]'s contents has not been consumed.
+ */
+class EventObserver<T>(private val onEventUnconsumedContent: (T) -> Unit) : Observer<Event<T>> {
+    override fun onChanged(event: Event<T>?) {
+        event?.consume()?.let { value ->
+            onEventUnconsumedContent(value)
+        }
+    }
+}
