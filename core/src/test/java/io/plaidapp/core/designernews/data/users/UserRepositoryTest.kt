4d427cc4fbeed92d89c6587f92b6d34bbdb547ee
Florina Muntenescu <florinam@google.com>
2018-07-11 09:47:41 +0100

    Tests added and code formatted

--- core/src/test/java/io/plaidapp/core/designernews/data/users/UserRepositoryTest.kt
+++ core/src/test/java/io/plaidapp/core/designernews/data/users/UserRepositoryTest.kt
@@ -16,15 +16,93 @@
 
 package io.plaidapp.core.designernews.data.users
 
+import io.plaidapp.core.data.Result
 import io.plaidapp.core.designernews.data.api.DesignerNewsService
+import io.plaidapp.core.designernews.data.api.errorResponseBody
+import io.plaidapp.core.designernews.data.api.model.User
+import io.plaidapp.core.designernews.data.api.user1
+import io.plaidapp.core.designernews.data.api.user2
+import io.plaidapp.core.designernews.data.api.users
+import kotlinx.coroutines.experimental.CompletableDeferred
+import kotlinx.coroutines.experimental.runBlocking
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
 import org.junit.Test
 import org.mockito.Mockito
+import retrofit2.Response
 
+/**
+ * Test for [UserRepository] that mocks all the dependencies.
+ */
 class UserRepositoryTest {
 
     private val service = Mockito.mock(DesignerNewsService::class.java)
+    private val repository = UserRepository(service)
 
     @Test
-    fun getUsers() {
+    fun getUsers_withNoCachedUsers_withSuccess() = runBlocking {
+        // Given that the service responds with success
+        withUsersSuccess("111,222", users)
+
+        // When requesting the users
+        val result = repository.getUsers(setOf(111L, 222L))
+
+        // Then there's one request to the service
+        Mockito.verify(service).getUsers("111,222")
+        // Then the correct set of users is returned
+        assertEquals(Result.Success(users.toSet()), result)
+    }
+
+    @Test
+    fun getUsers_withNoCachedUsers_withError() = runBlocking {
+        // Given that the service responds with error
+        withUsersError("111,222")
+
+        // When requesting the users
+        val result = repository.getUsers(setOf(111L, 222L))
+
+        // Then error is returned
+        assertTrue(result is Result.Error)
+    }
+
+    @Test
+    fun getUsers_withCachedUsers_withSuccess() = runBlocking {
+        // Given a user that was already requested and cached
+        withUsersSuccess("111", listOf(user1))
+        repository.getUsers(setOf(111L))
+        // Given another user that can be requested
+        withUsersSuccess("222", listOf(user2))
+
+        // When requesting a list of users
+        val result = repository.getUsers(setOf(111L, 222L))
+        // Then there's one request to the service
+        Mockito.verify(service).getUsers("222")
+        // Then the correct set of users is returned
+        assertEquals(Result.Success(users.toSet()), result)
+    }
+
+    @Test
+    fun getUsers_withCachedUsers_withError() = runBlocking {
+        // Given a user that was already requested and cached
+        withUsersSuccess("111", listOf(user1))
+        repository.getUsers(setOf(111L))
+        // Given that the service responds with error for another users
+        withUsersError("222")
+
+        // When requesting the users
+        val result = repository.getUsers(setOf(111L, 222L))
+
+        // We get the cached user
+        assertEquals(Result.Success(setOf(user1)), result)
+    }
+
+    private fun withUsersSuccess(ids: String, users: List<User>) {
+        val result = Response.success(users)
+        Mockito.`when`(service.getUsers(ids)).thenReturn(CompletableDeferred(result))
+    }
+
+    private fun withUsersError(ids: String) {
+        val result = Response.error<List<User>>(400, errorResponseBody)
+        Mockito.`when`(service.getUsers(ids)).thenReturn(CompletableDeferred(result))
     }
 }
