5c09cae244aea0460d235ed131cae3e93bc8fd1f
Florina Muntenescu <florinam@google.com>
2018-07-13 16:19:31 +0100

    Moving CommentViewHolder binding logic away from the Activity

--- core/src/main/java/io/plaidapp/core/designernews/data/api/model/Story.kt
+++ core/src/main/java/io/plaidapp/core/designernews/data/api/model/Story.kt
@@ -39,7 +39,7 @@ data class Story(
     @SerializedName("comment_count") val commentCount: Int = 0,
     @SerializedName("vote_count") val voteCount: Int = 0,
     @SerializedName("user_id") val userId: Long = 0L,
-    @SerializedName("created_at") val createdAt: Date? = null,
+    @SerializedName("created_at") val createdAt: Date,
     @SerializedName("links") val links: StoryLinks? = null,
     @Deprecated("Removed in DN API V2")
     @SerializedName("user_display_name") val userDisplayName: String? = null,
