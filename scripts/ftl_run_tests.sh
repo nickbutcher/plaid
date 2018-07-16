042df2ccbc661539e38a6ebbe6530b0ac476caf9
Tiem Song <tiem@google.com>
2018-07-15 21:54:14 -0700

    Add scripts for gcloud auth and running tests

---
+++ scripts/ftl_run_tests.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+BUILD_ID=$1
+TEST_DIR=$2
+
+test_apk() {
+	RESULTS_DIR=$1_$BUILD_ID
+
+    gcloud firebase test android run \
+	    --type instrumentation \
+	    --app $2 \
+	    --test $3 \
+	    --device model=Nexus6P,version=27,locale=en_US,orientation=portrait \
+	    --timeout 30m \
+	    --results-bucket cloud-test-android-devrel-ci \
+	    --results-dir=$RESULTS_DIR \
+	    --no-record-video \
+	    --no-performance-metrics
+
+    # Make result dir
+    mkdir -p "$TEST_DIR/$RESULTS_DIR"
+
+	# Pull down test results
+	gsutil -m cp -r -U "gs://TODO-PROJECT-ID/$RESULTS_DIR/*" "$TEST_DIR/$RESULTS_DIR"
+}
+
+test_apk \
+	"app" \
+	"app/build/outputs/apk/debug/app-debug.apk" \
+	"app/build/outputs/apk/androidTest/debug/app-debug-androidTest.apk"
