8fb0c153741e153391abb0b2be768a53eaaf94fe
Florina Muntenescu <florinam@google.com>
2018-07-18 11:24:44 +0100

    Moving LoginViewModel to designernews

--- designernews/src/main/java/io/plaidapp/designernews/ui/login/LoginViewModel.kt
+++ designernews/src/main/java/io/plaidapp/designernews/ui/login/LoginViewModel.kt
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package io.plaidapp.core.designernews.ui.login
+package io.plaidapp.designernews.ui.login
 
 import android.arch.lifecycle.LiveData
 import android.arch.lifecycle.MutableLiveData
