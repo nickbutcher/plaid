3e0ab96b45c471a5a8cdcce800ba7c1d97704e9f
Tiem Song <tiem@google.com>
2018-07-26 21:14:11 -0700

    Convert AuthInterceptor to Kotlin

--- core/src/main/java/io/plaidapp/core/data/api/AuthInterceptor.kt
+++ core/src/main/java/io/plaidapp/core/data/api/AuthInterceptor.kt
@@ -15,30 +15,20 @@
  *
  */
 
-package io.plaidapp.core.data.api;
+package io.plaidapp.core.data.api
 
-import java.io.IOException;
-
-import okhttp3.Interceptor;
-import okhttp3.Request;
-import okhttp3.Response;
+import okhttp3.Interceptor
+import okhttp3.Response
+import java.io.IOException
 
 /**
  * A {@see RequestInterceptor} that adds an auth token to requests
  */
-public class AuthInterceptor implements Interceptor {
+class AuthInterceptor(private val accessToken: String) : Interceptor {
 
-    private final String accessToken;
-
-    public AuthInterceptor(String accessToken) {
-        this.accessToken = accessToken;
-    }
-
-    @Override
-    public Response intercept(Chain chain) throws IOException {
-        final Request request = chain.request().newBuilder()
-                .addHeader("Authorization", "Bearer " + accessToken)
-                .build();
-        return chain.proceed(request);
+    @Throws(IOException::class)
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val request = chain.request().newBuilder().addHeader("Authorization", "Bearer $accessToken").build()
+        return chain.proceed(request)
     }
 }
