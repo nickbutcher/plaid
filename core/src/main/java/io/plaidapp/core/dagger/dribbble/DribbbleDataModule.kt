94c11f92650b03b0a26e36b38f7a2fac8a1052be
Ben Weiss <benweiss@google.com>
2018-09-19 16:19:48 +0100

    Wire up dagger components
    CoreComponent gets injected into HomeComponent and DribbbleComponent.
    This makes it provide a true singleton of the modules declared there.
    
    Several @Provides functions are now provision functions (without
    parameter) so component dependency resolution works without
    subcomponents.

--- core/src/main/java/io/plaidapp/core/dagger/dribbble/DribbbleDataModule.kt
+++ core/src/main/java/io/plaidapp/core/dagger/dribbble/DribbbleDataModule.kt
@@ -16,14 +16,53 @@
 
 package io.plaidapp.core.dagger.dribbble
 
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.experimental.CoroutineCallAdapterFactory
 import dagger.Module
 import dagger.Provides
+import io.plaidapp.core.BuildConfig
+import io.plaidapp.core.dagger.CoroutinesContextProviderModule
+import io.plaidapp.core.data.CoroutinesContextProvider
+import io.plaidapp.core.dribbble.data.ShotsRepository
+import io.plaidapp.core.dribbble.data.search.DribbbleSearchConverter
 import io.plaidapp.core.dribbble.data.search.DribbbleSearchService
+import io.plaidapp.core.dribbble.data.search.SearchRemoteDataSource
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 
-@Module(includes = [DribbbleRetrofitModule::class])
+/**
+ * Dagger module providing classes required to dribbble with data.
+ */
+@Module(includes = [CoroutinesContextProviderModule::class])
 class DribbbleDataModule {
 
-    @Provides fun provideDribbbleSearchService(retrofit: Retrofit): DribbbleSearchService =
-        retrofit.create(DribbbleSearchService::class.java)
+    @Provides fun provideShotsRepository(
+        remoteDataSource: SearchRemoteDataSource,
+        contextProvider: CoroutinesContextProvider
+    ) = ShotsRepository.getInstance(remoteDataSource, contextProvider)
+
+    @Provides fun provideDribbbleSearchService(): DribbbleSearchService =
+        provideRetrofit().create(DribbbleSearchService::class.java)
+
+    @Provides fun provideRetrofit(): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(DribbbleSearchService.ENDPOINT)
+            .addConverterFactory(DribbbleSearchConverter.Factory())
+            .addCallAdapterFactory(CoroutineCallAdapterFactory())
+            .client(provideOkHttpClient(provideLoggingInterceptor()))
+            .build()
+    }
+
+    @Provides fun provideLoggingInterceptor(): HttpLoggingInterceptor {
+        return HttpLoggingInterceptor().apply {
+            level = if (BuildConfig.DEBUG) {
+                HttpLoggingInterceptor.Level.BODY
+            } else {
+                HttpLoggingInterceptor.Level.NONE
+            }
+        }
+    }
+
+    @Provides fun provideOkHttpClient(loggingInterceptor: HttpLoggingInterceptor): OkHttpClient =
+        OkHttpClient.Builder().addInterceptor(loggingInterceptor).build()
 }
