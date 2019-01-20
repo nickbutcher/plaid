960b65bb0cc309416b1ed632dfee08494a5c3744
Ataul Munim <hello@ataulm.com>
2019-01-20 23:47:24 +0000

    Run spotless

--- core/src/main/java/io/plaidapp/core/designernews/data/api/StoryIds.kt
+++ core/src/main/java/io/plaidapp/core/designernews/data/api/StoryIds.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2018 Google, Inc.
+ * Copyright 2019 Google, Inc.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package io.plaidapp.core.designernews.data.api
 
 private const val STORY_ID_SEPARATOR = ","
