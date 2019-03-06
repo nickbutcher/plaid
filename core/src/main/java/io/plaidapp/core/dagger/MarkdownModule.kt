34100c6937cff8720f942e7fdad25772fc5fa7ed
Ben Weiss <benweiss@google.com>
2019-03-06 11:06:21 +0000

    Clean up unneccessary @Inject and import

--- core/src/main/java/io/plaidapp/core/dagger/MarkdownModule.kt
+++ core/src/main/java/io/plaidapp/core/dagger/MarkdownModule.kt
@@ -21,12 +21,11 @@ import `in`.uncod.android.bypass.Markdown
 import android.util.DisplayMetrics
 import dagger.Module
 import dagger.Provides
-import javax.inject.Inject
 
 /**
  * Provide [Markdown] to this app's components.
  */
-@Module class MarkdownModule @Inject constructor(
+@Module class MarkdownModule constructor(
     private val displayMetrics: DisplayMetrics,
     private val options: Bypass.Options = Bypass.Options()
 ) {
