1822e96659ad79122b11d24a1d6de6cdc8216796
Florina Muntenescu <florinam@google.com>
2019-03-10 10:07:28 +0100

    Remove unused methods

--- core/src/main/java/io/plaidapp/core/dribbble/data/search/DribbbleSearchService.kt
+++ core/src/main/java/io/plaidapp/core/dribbble/data/search/DribbbleSearchService.kt
@@ -16,11 +16,8 @@
 
 package io.plaidapp.core.dribbble.data.search
 
-import androidx.annotation.StringDef
-
 import io.plaidapp.core.dribbble.data.api.model.Shot
 import kotlinx.coroutines.Deferred
-import retrofit2.Call
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -30,15 +27,6 @@ import retrofit2.http.Query
  */
 interface DribbbleSearchService {
 
-    @Deprecated("Move to DribbbleSearchService#searchDeferred")
-    @GET("search")
-    fun search(
-        @Query("q") query: String,
-        @Query("page") page: Int?,
-        @Query("per_page") pageSize: Int,
-        @Query("s") @SortOrder sort: String
-    ): Call<List<Shot>>
-
     @GET("search")
     fun searchDeferred(
         @Query("q") query: String,
@@ -47,18 +35,9 @@ interface DribbbleSearchService {
         @Query("per_page") pageSize: Int
     ): Deferred<Response<List<Shot>>>
 
-    @kotlin.annotation.Retention(AnnotationRetention.SOURCE)
-    @StringDef(
-        SORT_POPULAR,
-        SORT_RECENT
-    )
-    annotation class SortOrder
-
     companion object {
 
         const val ENDPOINT = "https://dribbble.com/"
-        const val SORT_POPULAR = ""
-        const val SORT_RECENT = "latest"
         const val PER_PAGE_DEFAULT = 12
     }
 }
